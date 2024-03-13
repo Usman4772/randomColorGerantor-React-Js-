e4937f835d6916ff95cd3c9c2b6191c8743f7936
usman ali <usmanali477275@gmail.com>
2024-03-13 21:28:49 +0500

    Random color Generator In React Js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const buttonStyle={
+    padding:".5rem 1rem",
+    border:"none",
+    outline:"none",
+    cursor:"pointer",
+    borderRadius:".3rem"
+  }
+  const [type,setType]=useState("hex")
+  const [color,setColor]=useState("#000000")
+  function createRandomIndex(length){
+    let index=Math.floor(Math.random()*length)
+    return index
+  }
+  function createRandomHexColor(){
+const hex=[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+let color="#"
+for(let i=0;i<6;i++){
+  color+=hex[createRandomIndex(hex.length)]
+}
+setColor(color)
+  }
+  function createRandomRbgColor(){
+    let r=createRandomIndex(255)
+    let g=createRandomIndex(255)
+    let b=createRandomIndex(255)
+    setColor(`rgb(${r},${g},${b})`)
+
+  } 
+   return (
+  
+    <div style={{
+      width:"100vw",
+      height:"100vh",
+      background:color,
+      padding:"2rem 0",
+    }}>
+    <div style={{
+      width:"100vw",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      padding:"2rem 0",
+      gap:"1rem"
+    }} >
+    <button style={buttonStyle}  onClick={()=>setType("hex")}>Create hex Color</button>
+    <button style={buttonStyle} onClick={()=>setType("rgb")}>Create rgb Color</button>
+    <button style={buttonStyle} onClick={type==="hex"?createRandomHexColor:createRandomRbgColor}>Create Random Color</button>      
+    </div>
+    <div style={{color:"white",display:"flex",justifyContent:"center",alignItems:"center", width:"100vw",height:"100vh",fontSize:"3rem"}}>{color}</div>
     </div>
   );
 }
